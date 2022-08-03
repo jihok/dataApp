ae37467d72de248690ca5a79dae65697d46e6190
jihok <1700785+jihok@users.noreply.github.com>
2022-08-03 12:02:53 -0400

    add custom theme

--- select_component/select.py
+++ select_component/select.py
@@ -1,6 +1,11 @@
 import os
 import streamlit.components.v1 as components
 
+# _component_func = components.declare_component(
+#     "select",
+#     url="http://localhost:3001",
+# )
+
 parent_dir = os.path.dirname(os.path.abspath(__file__))
 build_dir = os.path.join(parent_dir, "build")
 _component_func = components.declare_component(
